148af90415b50a7525d434f03a2bee1ce3a2e3f0
qingping95 <qingping95@gmail.com>
2016-04-30 22:28:25 +0800

    add Topcode pulgin

---
+++ topcoder/moj/template.cpp
@@ -0,0 +1,40 @@
+// Paste me into the FileEdit configuration dialog
+// Paste me into the FileEdit configuration dialog
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <climits>
+#include <deque>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <ctime>
+#include <list>
+#include <map>
+#include <set>
+#include <utility>
+#include <sstream>
+#include <complex>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <bitset>
+#include <functional>
+#include <algorithm>
+typedef long long LL;
+using namespace std;
+
+
+class $CLASSNAME$ {
+public:
+   $RC$ $METHODNAME$( $METHODPARMS$ ) {
+
+   }
+};
+
+$BEGINCUT$
+$TESTCODE$
+
+$DEFAULTMAIN$
+$ENDCUT$
