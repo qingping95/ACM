d7b02accf3289a7555c0b1410d4053d7e9d19053
Qingping Yang <qingping95@gmail.com>
2021-03-07 15:45:36 +0800

    update algorithm of calculating prime numbers

--- 模板库/自整理/数论/朴素筛素数.cpp
+++ 模板库/自整理/数论/朴素筛素数.cpp
@@ -13,9 +13,8 @@ void get_prime()
         }
     }
 }
-//ÆÓËØÉ¸ËØÊı
 
-//--------------------O(n)É¸·¨------------------------//
+//--------------------O(n)ç­›æ³•------------------------//
 bool pvis[N];
 int pri[N];
 int prin;
@@ -29,4 +28,108 @@ void getpri(){
             if(i % pri[j] == 0) break;
         }
     }
+}
+
+// ----------------------é¢‘ç¹åˆ†è§£è´¨å› æ•°æ—¶ä½¿ç”¨ï¼Œæœ€ä¼˜ç®—æ³•------------------------------- // 
+
+#include <iostream>
+#include <cstdio>
+#include <stack>
+#include <cstring>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <bitset>
+#include <set>
+#include <map>
+#include <unordered_map>
+#define lson x<<1
+#define rson x<<1|1
+#define mid ((lt[x].l+lt[x].r)/2)
+#define mkpr make_pair
+//#define ID(x, y) ((x)*m+(y))
+//#define CHECK(x, y) ((x)>=0 && (x)<n && (y)>=0 && (y)<m)
+using namespace std;
+typedef long long LL;
+typedef pair<LL,LL> PII;
+const int mod = 1e9+7;
+const int INF=0x3f3f3f3f;
+const int N = 200010;
+set<pair<int, int> > plist[N];
+int gcd_pc[N];
+int nxt[N];
+unordered_map<int, int> pcount[N];
+void get_prime()
+{
+	for (int i = 2; i < N; ++i) {
+        if (nxt[i] == 0) {
+            nxt[i] = i;
+            if (i > 10000) continue;
+            for (int j = i * i; j < N; j += i) {
+                if (nxt[j] == 0) nxt[j] = i;
+            }
+        }
+    }
+}
+
+int main()
+{
+	get_prime();
+	int n, q;
+	scanf("%d%d", &n, &q);
+	int ans = 1;
+	for(int i = 1; i <= n; i++)
+	{
+		int x;
+		scanf("%d", &x);
+		if(i == 1) ans = x;
+		else ans = __gcd(ans, x);
+		while(x != 1)
+		{
+			int pri = nxt[x];
+			int cnt = 0;
+			while(nxt[x] == pri)
+			{
+				cnt += 1;
+				x /= nxt[x];
+			}
+			if(cnt == 0) continue;
+			plist[pri].insert(PII(cnt, i));
+			pcount[i][pri] = cnt;
+		}
+	}
+	while(q--)
+	{
+		int idx, x;
+		scanf("%d%d", &idx, &x);
+		while(x != 1)
+		{
+			int pri = nxt[x];
+			int cnt = 0;
+			while(nxt[x] == pri)
+			{
+				cnt += 1;
+				x /= nxt[x];
+			}
+			if(cnt == 0) continue;
+			int gcd_cnt = (int)plist[pri].size() == n ? plist[pri].begin()->first : gcd_pc[pri];
+			int old_cnt = pcount[idx][pri];
+			int new_cnt = old_cnt + cnt;
+			plist[pri].erase(mkpr(old_cnt, idx));
+			plist[pri].insert(mkpr(new_cnt, idx));
+			pcount[idx][pri] = new_cnt;
+			if((int)plist[pri].size() == n){
+				int cur_gcd_cnt = plist[pri].begin()->first;
+				for(int k = gcd_cnt; k < cur_gcd_cnt; k++)
+				{
+					ans = (LL)ans * pri % mod;
+				}
+				while(plist[pri].begin()->first == cur_gcd_cnt) plist[pri].erase(plist[pri].begin());
+				gcd_pc[pri] = cur_gcd_cnt;
+			}
+		}
+		printf("%d\n", ans);
+	}
+	
+	return 0;
 }
